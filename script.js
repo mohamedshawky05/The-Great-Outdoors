ee994eba0eb626b754154fbac36ebdbb55931075
mohamedshawky05 <mohamedshawky0981@gmail.com>
2024-10-23 16:12:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript can be used here for interactivity, such as showing a video when the play button is clicked.
+document.querySelector('.play-btn').addEventListener('click', () => {
+    alert('Play button clicked! Implement video here.');
+});
